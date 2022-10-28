6e017a4ec299c751cbee547ab7507ed24fae58ac
HangyuanYang <690094231@qq.com>
2022-10-28 22:50:13 +0800

    model_evaluation() func in utils.py

--- src/utils.py
+++ src/utils.py
@@ -43,3 +43,19 @@ def preprocess(df):
     return df
 
 
+def model_evaluation(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    categories  = ['Negative','Positive']
+    group_names = ['True Neg','False Pos', 'False Neg','True Pos']
+    group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+
+    labels = [f'{v1}\n{v2}' for v1, v2 in zip(group_names,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+
+    sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',xticklabels = categories, yticklabels = categories)
+
+    plt.xlabel("Predicted values", fontdict = {'size':15}, labelpad = 10)
+    plt.ylabel("Actual values"   , fontdict = {'size':15}, labelpad = 10)
+    plt.title ("Confusion Matrix", fontdict = {'size':20}, pad = 20)
